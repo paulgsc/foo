67aa2a045b01196bc4fec2bd2920ac99844cf886
Paul Gathondu <aulgondu@gmail.com>
2024-12-31 13:20:41 -0800

    fix: wrong fields

--- migrations/20241229062042_create_table_backie.down.sql
+++ migrations/20241229062042_create_table_backie.down.sql
@@ -1,9 +1,11 @@
 -- Drop the indexes explicitly
-DROP INDEX IF EXISTS idx_backie_tasks_queue_state;
-
-DROP INDEX IF EXISTS idx_backie_tasks_state;
-
 DROP INDEX IF EXISTS idx_backie_tasks_scheduled;
+
+DROP INDEX IF EXISTS idx_backie_tasks_task_name;
+
+DROP INDEX IF EXISTS idx_backie_tasks_queue_name;
+
+DROP INDEX IF EXISTS idx_backie_tasks_task_queue;
 
 -- Add down migration script here
 DROP TABLE IF EXISTS backie_tasks;
--- migrations/20241229062042_create_table_backie.up.sql
+++ migrations/20241229062042_create_table_backie.up.sql
@@ -3,22 +3,23 @@ CREATE TABLE backie_tasks (
   id TEXT PRIMARY KEY NOT NULL,
   task_name TEXT NOT NULL,
   queue_name TEXT NOT NULL,
-  uniq_has TEXT,
+  uniq_hash TEXT,
   payload TEXT NOT NULL,
   timeout_msecs INTEGER NOT NULL,
-  state TEXT NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  scheduled_at TIMESTAMP NOT NULL,
-  running_at TIMESTAMP,
-  done_at TIMESTAMP,
+  created_at INTEGER NOT NULL,
+  scheduled_at INTEGER NOT NULL,
+  running_at INTEGER,
+  done_at INTEGER,
   error_info TEXT,
   retries INTEGER NOT NULL DEFAULT 0,
   max_retries INTEGER NOT NULL,
   backoff_mode TEXT NOT NULL
 );
 
-CREATE INDEX idx_backie_tasks_queue_state ON backie_tasks (queue_name, state);
-
-CREATE INDEX idx_backie_tasks_state ON backie_tasks (state);
-
 CREATE INDEX idx_backie_tasks_scheduled ON backie_tasks (scheduled_at);
+
+CREATE INDEX idx_backie_tasks_task_name ON backie_tasks (task_name);
+
+CREATE INDEX idx_backie_tasks_queue_name ON backie_tasks (queue_name);
+
+CREATE INDEX idx_backie_tasks_task_queue ON backie_tasks (task_name, queue_name);
